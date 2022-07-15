02aadd59615642e30d37dfda2ba519db2ff1e707
Alireza <arnn379@gmail.com>
2022-07-15 21:38:11 +0430

    Relational Models added

--- eTickets/Program.cs
+++ eTickets/Program.cs
@@ -1,4 +1,9 @@
+using eTickets.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
+//DbContext coniguration
+builder.Services.AddDbContext<AppDbContext>();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
