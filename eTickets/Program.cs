1b3397d6c477b19036115d80fc14b584e51f9754
Alireza <arnn379@gmail.com>
2022-07-15 23:52:39 +0430

    sql server configured

--- eTickets/Program.cs
+++ eTickets/Program.cs
@@ -1,9 +1,11 @@
 using eTickets.Data;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 //DbContext coniguration
-builder.Services.AddDbContext<AppDbContext>();
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString
+    ("DefaultConnectionString")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
