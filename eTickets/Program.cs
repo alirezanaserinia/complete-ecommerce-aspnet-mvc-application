5333bb1277ece2a1ec91aea548922a99e104aeb6
Alireza <arnn379@gmail.com>
2022-07-20 00:59:27 +0430

    Work With Cinema Data Added

--- eTickets/Program.cs
+++ eTickets/Program.cs
@@ -11,6 +11,7 @@ builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(buil
 //Services configuration
 builder.Services.AddScoped<IActorsService, ActorsService>();
 builder.Services.AddScoped<IProducersService, ProducersService>();
+builder.Services.AddScoped<ICinemasService, CinemasService>();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
